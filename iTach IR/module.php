98d36b8ad8cdd83b14935967b6fd16732f75c970
stigkristiansen <stigkristiansen@outlook.com>
2021-12-31 00:28:20 +0100

    Update module.php

--- iTach IR/module.php
+++ iTach IR/module.php
@@ -1,39 +1,84 @@
 <?php
 
 declare(strict_types=1);
-	class iTachDevice extends IPSModule
-	{
-		public function Create()
-		{
-			//Never delete this line!
-			parent::Create();
+class iTachDevice extends IPSModule {
+	public function Create() {
+		//Never delete this line!
+		parent::Create();
 
-			$this->ForceParent('{3CFF0FD9-E306-41DB-9B5A-9D06D38576C3}');
+		$this->ForceParent('{3CFF0FD9-E306-41DB-9B5A-9D06D38576C3}');
 
-			$this->RegisterPropertyString('IPAddress', '');
-			$this->RegisterPropertyString('Model', '');
-			$this->RegisterPropertyString('Name', '');
-		}
+		$this->RegisterPropertyString('IPAddress', '');
+		$this->RegisterPropertyString('Model', '');
+		$this->RegisterPropertyString('Name', '');
 
-		public function Destroy()
-		{
-			//Never delete this line!
-			parent::Destroy();
-		}
+		$this->RegisterTimer('SetIOConfig', 0, 'IPS_RequestAction(' . (string)$this->InstanceID . ', "SetIOConfig", 0);'); 
+	}
 
-		public function ApplyChanges()
-		{
-			//Never delete this line!
-			parent::ApplyChanges();
-		}
+	public function Destroy() {
+		//Never delete this line!
+		parent::Destroy();
+	}
 
-		public function ReceiveData($JSONString)
-		{
-			$data = json_decode($JSONString);
-			IPS_LogMessage('Device RECV', utf8_decode($data->Buffer));
-		}
+	public function ApplyChanges() {
+		//Never delete this line!
+		parent::ApplyChanges();
 
-		public function SendIRCommand(string $Device, string $Command) {
+		$this->RegisterMessage(0, IPS_KERNELMESSAGE);
 
+		if (IPS_GetKernelRunlevel() == KR_READY) {
+			$this->Init();
 		}
 	}
+
+	public function MessageSink($TimeStamp, $SenderID, $Message, $Data) {
+		parent::MessageSink($TimeStamp, $SenderID, $Message, $Data);
+
+		$this->SendDebug(__FUNCTION__, sprintf('Received a message: %d - %d - %d', $SenderID, $Message, $data[0]), 0);
+
+		if ($Message == IPS_KERNELMESSAGE && $Data[0] == KR_READY) {
+			$this->LogMessage('Detected "Kernel Ready"!', KL_NOTIFY);
+			$this->Init();
+		}
+	}
+
+	private function Init() {
+		$msg = 'Initializing...';
+		
+		$this->LogMessage($msg, KL_NOTIFY);
+		$this->SendDebug(__FUNCTION__, $msg, 0);
+
+		$this->SetTimerInterval('SetIOConfig', 1000);
+	}
+
+	public function RequestAction($Ident, $Value) {
+		switch (strtolower($Ident)) {
+			case 'setioconfig':
+				$this->SetIOConfig();
+				break;
+			}
+	}
+
+	private function SetIOConfig() {
+		$this->SendDebug(__FUNCTION__, 'Setting the configuration of the Socket Client I/O instance...', 0);
+
+		$this->SetTimerInterval('SetIOConfig', 0);
+		
+		$parentId = IPS_GetInstance($this->InstanceID)['ConnectionID'];
+		
+		IPS_SetProperty($parentId, 'Host', $this->ReadPropertyString('IPAddress'));
+		IPS_SetProperty($parentId, 'Port', 4998);
+		IPS_SetProperty($parentId, "Open", true);
+		IPS_ApplyChanges($parentId);
+	}
+
+	public function ReceiveData($JSONString)
+	{
+		$data = json_decode($JSONString);
+		IPS_LogMessage('Device RECV', utf8_decode($data->Buffer));
+	}
+
+	public function SendIRCommand(string $Device, string $Command) {
+
+	}
+}
