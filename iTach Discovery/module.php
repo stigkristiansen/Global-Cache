6f9b8708d445af5c694f5b50c19cec8d367041e8
stigkristiansen <stigkristiansen@outlook.com>
2021-12-30 19:23:33 +0100

    Update module.php

--- iTach Discovery/module.php
+++ iTach Discovery/module.php
@@ -85,7 +85,7 @@ class iTachDiscovery extends IPSModule {
 			return;
 		}
 
-		$multicast = substr($multicast, $pre+4, $post-$pre+4);
+		$multicast = substr($multicast, $pre+4, $post-$pre+3);
 
 		$multicast = str_replace('&lt;-', ';', $multicast);
 		$multicast = str_replace('&gt;', '', $multicast);
