c36ef9e4664628d55748187bd4643f394872e026
stigkristiansen <stigkristiansen@outlook.com>
2022-01-01 00:32:07 +0100

    .

--- iTach IR/module.php
+++ iTach IR/module.php
@@ -63,26 +63,43 @@ class iTachDevice extends IPSModule {
 	public function ReceiveData($JSONString)
 	{
 		$data = json_decode($JSONString);
-		//IPS_LogMessage('Device RECV', utf8_decode($data->Buffer));
+		
+		$this->SendDebug(__FUNCTION__, sprintf('Received data: %s', utf8_decode($data->Buffer)), 0);
 	}
 
 	public function SendIRCommand(string $Device, string $Command) {
-		$irCodes = $this->ReadPropertyString('IRCodes');
+		$codes = json_decode($this->ReadPropertyString('IRCodes'), true);
+				
+		$device = strtolower($Device);
+		$command = strtolower($Command);
+		
+		$buffer='';
+		foreach($codes as $code) {
+			if(strtolower($code['Device'])==$device && strtolower($code['Command'])==$command) {
+				$buffer = sprintf("sendir,%s,%d,%s",$this->ReadPropertyString("Port"), $this->InstanceID, $code['Code']);
+				$buffer .= chr(13).chr(10);
+				break;
+			}
+		}
 
-		$this->SendDebug(__FUNCTION__, 'IR Codes: '.$irCodes, 0);
+		if(strlen($buffer)>0) {
+			try {
+				$this->SendDataToParent(json_encode(Array("DataID" => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}", "Buffer" => $buffer)));
+				return true;
+			} catch (Exeption $ex) {
+				$msg = sprintf('Failed to send the command %s:%s. Error: %S',$Device, $Command, $ex->getMessage());
+				
+				$this->LogMessage($msg, KL_ERROR);
+				$this->SendDebug(__FUNCTION__, $msg, 0);
 
-		return;
-
-		$buffer = "sendir,".$this->ReadPropertyString("Port").",".IPS_GetParent($cId).",".GetValueString($vId).chr(13).chr(10);
-		try{
-			$this->SendDataToParent(json_encode(Array("DataID" => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}", "Buffer" => $buffer)));
-		} catch (Exeption $ex) {
-			$msg = sprintf('Failed to send the command %s:%s. Error: %S',$Device, $Command, $ex->getMessage());
-			
-			$log->LogMessage($msg, KL_ERROR);
-			
+				return false;
+			}
+		} else {
+			$this->SendDebug(__FUNCTION__, sprintf('Unknown "Device" and/or "Command" (%s:%s)', $Device, $Command), 0);
 			return false;
 		}
+		
+		
 		
 		return true;
 	 
